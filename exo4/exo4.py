4fee6f42f40f4e230cd47204eb36f65383d125a3
Alexandre Barde <alexandre.barde@thalesaleniaspace.com>
2019-11-08 11:32:02 +0100

    exo4 part 4

--- exo4/exo4.py
+++ exo4/exo4.py
@@ -1,5 +1,6 @@
 import random
 
+
 def est_valide(chaine):
     splitted = list(chaine)
 
@@ -13,16 +14,27 @@ def est_valide(chaine):
             break
     return valeur
 
+
 def saisie():
     caracteres_valide = ['a', 't', 'g', 'c']
     chaine = ""
-    for i in range(4):
-            chaine = chaine + random.choice(caracteres_valide)
+    for i in range(2):
+        chaine = chaine + random.choice(caracteres_valide)
     return chaine
 
-print(saisie())
+
+def proportion(chaine, sequence):
+    splitted = chaine.split(sequence)
+    nombreOccurence = len(splitted)
+    pourcentage = (nombreOccurence / len(chaine)) * 100
+    return round(pourcentage, 2)
 
 chaineADN = "attgcaatggtggtacatg"
 
-print("Valide ? : " + str(est_valide(chaineADN)))
+print("Chaîne : " + str(chaineADN))
 
+char_saisie = saisie()
+
+print("Séquence : " + str(char_saisie))
+
+print("Il y a " + str(proportion(chaineADN, char_saisie)) + "% de \"" + str(char_saisie) + "\" dans votre chaîne.")
